d82e07349bba4159123c7e4e4ac2c5a6cb912670
GRinvest <grfx@live.ru>
2023-03-18 04:06:28 +0700

    add personal job

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from loguru import logger
 
 from config import config
 
-BASE_DIR = pathlib.Path(__file__).parent
-COIN = config.general.coin.lower()
-
 
 def module_from_file(module_name, file_path):
     spec = importlib.util.spec_from_file_location(module_name, file_path)
@@ -18,19 +15,10 @@ def module_from_file(module_name, file_path):
     return module
 
 
-MODULE = module_from_file(COIN, f"{BASE_DIR}/stratum/{COIN}.py")
+MODULE = module_from_file('server', f"{pathlib.Path(__file__).parent}/stratum/{config.general.coin.lower()}/server.py")
 
 
-async def job_manager(event: asyncio.Event):
-    while True:
-        await MODULE.state_updater()
-        await asyncio.sleep(0.3)
-        if not event.is_set():
-            event.set()
-
-
-async def run_proxy(event: asyncio.Event):
-    await event.wait()
+async def run_proxy():
     server = await asyncio.start_server(
         MODULE.handle_client,
         config.server.host,
@@ -41,14 +29,10 @@ async def run_proxy(event: asyncio.Event):
 
 
 async def execute():
-    event = asyncio.Event()
     while True:
         logger.success('Running Session Program')
         try:
-            await asyncio.gather(
-                asyncio.create_task(job_manager(event)),
-                asyncio.create_task(run_proxy(event))
-            )
+            await run_proxy()
         except Exception as e:
             logger.exception(e)
 
---
+++ stratum/evr/connector.py
@@ -0,0 +1,60 @@
+import asyncio
+
+import ujson
+
+from .state import TemplateState
+
+
+class ConnectorManager:
+
+    def __init__(self):
+        self.active_connectors: set[asyncio.StreamWriter] = set()
+
+    async def connect(self, writer: asyncio.StreamWriter, message: dict):
+        await self.send_personal_message(writer, None, True, message['id'])
+        await self.send_new_job(writer)
+        self.active_connectors.add(writer)
+
+    async def disconnect(self, writer):
+        if writer in self.active_connectors:
+            self.active_connectors.remove(writer)
+        if not writer.is_closing():
+            writer.close()
+            await writer.wait_closed()
+
+    async def send_personal_message(self,
+                                    writer: asyncio.StreamWriter,
+                                    method: str | None,
+                                    params: list | bool,
+                                    id_: int | None = None,
+                                    error: list | None = None):
+        if method:
+            s = ujson.dumps({'jsonrpc': '2.0', 'id': id_, 'method': method, 'params': params})
+        else:
+            s = ujson.dumps({'jsonrpc': '2.0', 'id': id_, 'error': error, 'result': params})
+        s += '\n'
+        if not writer.is_closing():
+            writer.write(s.encode())
+            try:
+                await asyncio.wait_for(writer.drain(), timeout=10)
+            except asyncio.TimeoutError:
+                if not writer.is_closing():
+                    writer.close()
+                    await writer.wait_closed()
+                if writer in self.active_connectors:
+                    self.active_connectors.remove(writer)
+
+    async def send_new_job(self, writer: asyncio.StreamWriter, state: TemplateState):
+        await self.send_personal_message(writer, 'mining.set_target', [state.target])
+        await self.send_personal_message(writer,
+                                         'mining.notify',
+                                         [hex(state.job_counter)[2:],
+                                          state.headerHash,
+                                          state.seedHash.hex(),
+                                          state.target,
+                                          True,
+                                          state.height,
+                                          state.bits])
+
+
+manager = ConnectorManager()
