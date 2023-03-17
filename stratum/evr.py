4662d5817199006e8d22482107d6bcc9c9209b57
GRinvest <grfx@live.ru>
2023-03-17 13:17:29 +0700

    add Error getting block template

--- stratum/evr.py
+++ stratum/evr.py
@@ -239,7 +239,7 @@ class Proxy:
                     break
                 j: dict = ujson.loads(data)
             except (TimeoutError, asyncio.TimeoutError):
-                if time() - self.time_block_fond > 4 * 60 * 60:
+                if time() - self.time_block_fond > 60 * 60:
                     break
                 else:
                     continue
