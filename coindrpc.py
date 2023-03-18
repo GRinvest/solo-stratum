8ab8677ac2ac87699ad9b1fa2ea36d99e90252c9
GRinvest <grfx@live.ru>
2023-03-19 05:18:36 +0700

    add personal job

--- coindrpc.py
+++ coindrpc.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import ujson
 from aiohttp import ClientSession
 from loguru import logger
@@ -9,6 +11,7 @@ class Coind:
     def __init__(self, username, password, port=5996, host='127.0.0.1'):
         self.url = f'http://{username}:{password}@{host}:{port}'
         self.id = 0
+        self.sem = asyncio.Semaphore(1)
 
     def __getattr__(self, name):
         if name.startswith('__') and name.endswith('__'):
@@ -16,26 +19,27 @@ class Coind:
             raise AttributeError
 
         async def ret(*args):
-            self.id += 1
-            data = {
-                'method': name,
-                'params': list(args),
-                'id': self.id,
-                'jsonrpc': '2.0',
-            }
-            async with ClientSession() as session:
-                async with session.post(self.url,
-                                        headers={'Content-Type': 'application/json'},
-                                        data=ujson.dumps(data)) as resp:
-                    try:
-                        json_obj = await resp.json()
-                        if json_obj.get('error', None):
-                            logger.error(f'coind error: {json_obj}')
-                            return json_obj.get('error', None)
-                    except Exception as e:
-                        logger.error(f'coind error: {e}')
-                    else:
-                        return json_obj
+            async with self.sem:
+                self.id += 1
+                data = {
+                    'method': name,
+                    'params': list(args),
+                    'id': self.id,
+                    'jsonrpc': '2.0',
+                }
+                async with ClientSession() as session:
+                    async with session.post(self.url,
+                                            headers={'Content-Type': 'application/json'},
+                                            data=ujson.dumps(data)) as resp:
+                        try:
+                            json_obj = await resp.json()
+                            if json_obj.get('error', None):
+                                logger.error(f'coind error: {json_obj}')
+                                return json_obj.get('error', None)
+                        except Exception as e:
+                            logger.error(f'coind error: {e}')
+                        else:
+                            return json_obj
 
         return ret
 
