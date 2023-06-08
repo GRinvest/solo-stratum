201a5832018f785ce0b65725ce6067ef0cf04867
GRinvest <grfx@live.ru>
2023-06-09 04:28:05 +0700

    add xna

--- stratum/xna/server.py
+++ stratum/xna/server.py
@@ -171,7 +171,7 @@ class Proxy:
                         break
                     j: dict = ujson.loads(data)
                 except (TimeoutError, asyncio.TimeoutError):
-                    if time() - self.time_block_fond > 60 * 5:
+                    if time() - self.time_block_fond > 60 * 10:
                         break
                     else:
                         continue
