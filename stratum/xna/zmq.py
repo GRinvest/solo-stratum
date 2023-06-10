e9ec2a2ba645cd4f995d5ef7c566e43992209d0b
GRinvest <grfx@live.ru>
2023-06-11 00:57:27 +0700

    add zmq

--- stratum/xna/zmq.py
+++ stratum/xna/zmq.py
@@ -1,3 +1,5 @@
+from time import time, strftime, gmtime
+
 import zmq
 import zmq.asyncio
 from loguru import logger
@@ -12,12 +14,15 @@ async def run():
     socket.setsockopt(zmq.RCVHWM, 0)
     socket.setsockopt_string(zmq.SUBSCRIBE, "hashblock")
     socket.connect("tcp://127.0.0.1:%i" % config.coind.zmq_port)
+    time_block = time()
     try:
         while True:
             _, body, _ = await socket.recv_multipart()
             EVENT_NEW_BLOCK.set()
-            logger.info(f'NEW BLOCK {body.hex()}')
+            delta = int(time() - time_block)
+            logger.info(f"NEW BLOCK {body.hex()} | потрачено минут {strftime('%M:%S', gmtime(delta))}")
             EVENT_NEW_BLOCK.clear()
+            time_block = time()
     finally:
         ctx.destroy()
 
