d6df6e121aa38967f75b979ea254fc088c5c0b51
GRinvest <grfx@live.ru>
2023-06-20 01:29:14 +0700

    add zmq

--- stratum/xna/server.py
+++ stratum/xna/server.py
@@ -143,7 +143,7 @@ class Proxy:
                         break
                     j: dict = ujson.loads(data)
                 except (TimeoutError, asyncio.TimeoutError):
-                    if time() - self.time_block_fond > 60 * 7:
+                    if time() - self.time_block_fond > 60 * 10:
                         break
                     else:
                         continue
